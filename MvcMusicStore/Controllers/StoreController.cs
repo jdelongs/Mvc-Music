7c20881be487a77030da2d70fc8c6ff0e9e9fac3
John Delong <jdelong15@hotmail.ca>
2017-12-12 04:22:25 -0800

    New Store View and Controller

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Store/Browse
+        public ActionResult Browse(string genre)
+        {
+            //add the selected genre to the viewbag so we can display it in the browse view
+            ViewBag.genre = genre; 
+            return View(); 
+        }
+    }
+}
